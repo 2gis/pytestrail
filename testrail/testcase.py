099dd45a8596dcf7d96cef560b68c248a024d7e8
Dmitry Osipov <osipovdf@gmail.com>
2014-08-27 14:58:53 +0700

    Renamed testcase`s 'title' attribute to match API name; Added attributes type_id and priority_id into TestRailTestCase.

--- testrail/testcase.py
+++ testrail/testcase.py
@@ -1,15 +1,17 @@
 class TestRailTestCase:
-    def __init__(self, name, section, suite, steps):
-        self.name = name
+    def __init__(self, title, section, suite, steps):
+        self.title = title
         self.section_name = section
         self.suite_name = suite
         self.steps = steps
+        self.type_id = 1
+        self.priority_id = 4
 
     def to_json_dict(self):
         jobject = {
-            'title': self.name,
-            'type_id': 1,
-            'priority_id': 4,
+            'title': self.title,
+            'type_id': self.type_id,
+            'priority_id': self.priority_id,
             'custom_steps_separated': self.steps
         }
 
