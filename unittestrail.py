770c68bb718cbf8bcbe0b6abb4111a8dd392d0be
Dmitry Osipov <osipovdf@gmail.com>
2014-09-02 10:53:27 +0700

    API fixes

--- unittestrail.py
+++ unittestrail.py
@@ -1,10 +1,10 @@
-import sys
-import json
+# coding=utf-8
 import argparse
 from os.path import expanduser, expandvars
 
 from source_files_parser import get_tests
 from testrail.testrail import APIClient
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -13,24 +13,26 @@ if __name__ == '__main__':
     parser.add_argument('-l', '--login', help='Testrail login', type=str)
     parser.add_argument('-P', '--password', help='Testrail password', type=str)
     parser.add_argument('-d', '--tests_dir', help='Tests directory', type=str, default='./tests')
-    args = parser.parse_args(sys.argv)
+    args = parser.parse_args()
 
-    cl = APIClient(args.base_url)
+    cl = APIClient(args.base_url, args.login, args.password)
     tests_dir = '/Users/ibakepunk/Projects/desktop4/tests/'
     tests_dir = expandvars(expanduser(args.tests_dir))
     project_id = args.project
 
     suites = {}
-    for x in json.loads(cl.get_suites(project_id)):
+    for x in cl.get_suites(project_id):
         suites[x['name']] = x['id']
 
     sections = {}
-    for x in json.loads(cl.get_sections(project_id, 0)):
-        sections[x['name']] = x['id']
+    for suite in suites.itervalues():
+        for x in cl.get_sections(project_id, suite):
+            sections[x['name']] = x['id']
 
     cases = {}
-    for x in json.loads(cl.get_all_cases(project_id)):
-        cases[x['title']] = x['id']
+    for suite in suites.itervalues():
+        for x in cl.get_all_cases(project_id, suite):
+            cases[x['title']] = x['id']
 
     testcases = get_tests(tests_dir)
 
@@ -38,7 +40,7 @@ if __name__ == '__main__':
     for x in testcases:
         actual_cases[x.title] = x
 
-    for case_title, case_id in cases:
+    for case_title, case_id in cases.iteritems():
         if case_title not in actual_cases:
             print 'Delete case: '+case_title
             cl.delete_case(case_id)
@@ -46,15 +48,15 @@ if __name__ == '__main__':
     for testcase in testcases:
         if testcase.suite_name not in suites:
             print 'Create suite: '+testcase.suite_name
-            new_suite = json.loads(cl.create_suite(project_id, testcase.suite_name))
+            new_suite = cl.create_suite(project_id, testcase.suite_name)
             suites[new_suite['name']] = new_suite['id']
 
         if testcase.section_name not in sections:
             print 'Create section "'+testcase.section_name+'" in suite "'+testcase.suite_name+'"'
-            new_section = json.loads(cl.create_section(project_id, testcase.suite_id, testcase.name))
+            new_section = cl.create_section(project_id, suites[testcase.suite_name], testcase.section_name)
             sections[new_section['name']] = new_section['id']
 
-        if testcase.name in cases:
+        if testcase.title in cases:
             print 'Update case: '+testcase.title
             cl.update_case(cases[testcase.title], testcase)
         else:
